20e9ba4ce4bf11e992aa618b62f67e30f1332aff
Sudara <sudara@alonetone.com>
2016-11-25 01:29:48 +0100

    Route the root to events#index

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   resources :regions
   resources :topics
   resources :categories
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'events#index'
 end
