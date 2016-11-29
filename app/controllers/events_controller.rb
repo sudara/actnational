b9d061911c7709f6c918b19b27c2a18ce3f1296d
Sudara <sudara@alonetone.com>
2016-11-29 04:33:22 +0100

    Fix preloads

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,7 +4,7 @@ class EventsController < ApplicationController
   # GET /events
   # GET /events.json
   def index
-    @regions = Region.all.preload(:future_events)
+    @regions = Region.all.preload(future_events: [:topic, :category] )
   end
 
   # GET /events/1
