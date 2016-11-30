3fffc558bec754420c2ce88ba1d1a94d095d87ac
Sudara <sudara@alonetone.com>
2016-11-30 03:51:20 +0100

    Populate some geolocation data via migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161130014828) do
+ActiveRecord::Schema.define(version: 20161130024120) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
