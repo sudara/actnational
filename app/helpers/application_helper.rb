8e1680e6e6f0778e06c28425f74b6725771a2937
Sudara <sudara@alonetone.com>
2016-12-25 19:10:46 -0800

    Add dynamic classes to the <body> tag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,9 @@ module ApplicationHelper
     markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, options)
     markdown.render(text).html_safe
   end
+  
+  def body_class
+    controller.action_name + "_" + 
+    controller.controller_name.gsub('_controller','').to_s.singularize
+  end
 end
